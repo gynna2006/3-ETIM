aa5de56af109decdd24093f3453c69b7cdc7c405
gynna2006 <giovannacolonisio@gmail.com>
2023-02-14 11:39:38 -0300

    Adicionado os arquivos estáticos, de dados, e classe de modelo

---
+++ Pokedex/Models/Tipo.cs
@@ -0,0 +1,11 @@
+namespace Pokedex.Models
+{
+    public class Tipo
+    {
+        // Atributos
+        public string Nome { get; set; }
+        
+        public string Cor { get; set; }
+
+    }
+}
